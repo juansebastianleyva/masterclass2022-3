770c37eb153294340c33bb007f7ec797b806c4f6
juansebastianleyva <jnleyva@gmail.com>
2022-09-24 18:40:02 -0500

    cambio codigo respuesta

--- src/main/java/com/usa/misiontic/masterclass3/controller/LibraryController.java
+++ src/main/java/com/usa/misiontic/masterclass3/controller/LibraryController.java
@@ -4,6 +4,7 @@ package com.usa.misiontic.masterclass3.controller;
 import com.usa.misiontic.masterclass3.entities.Library;
 import com.usa.misiontic.masterclass3.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class LibraryController {
         return libraryService.getAll();
     }
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Library save(@RequestBody  Library p){
         return libraryService.save(p);
     }
