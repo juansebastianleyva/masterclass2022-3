cee66ef39f232e8f5d988232e04cbd0a7652fc3e
juansebastianleyva <jnleyva@gmail.com>
2022-09-24 20:12:24 -0500

    Se agrega código de respuesta en controller

--- src/main/java/com/usa/misiontic/masterclass3/controller/ReservasController.java
+++ src/main/java/com/usa/misiontic/masterclass3/controller/ReservasController.java
@@ -4,12 +4,13 @@ package com.usa.misiontic.masterclass3.controller;
 import com.usa.misiontic.masterclass3.entities.Reservas;
 import com.usa.misiontic.masterclass3.service.ReservasService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reservas")
+@RequestMapping("/api/Reservas")
 public class ReservasController {
 
     @Autowired
@@ -20,6 +21,7 @@ public class ReservasController {
         return reservasService.getAll();
     }
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Reservas save(@RequestBody  Reservas p){
         return reservasService.save(p);
     }
