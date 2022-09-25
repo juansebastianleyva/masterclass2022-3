be692f34321f5286a6894cc06f50628db9ba749b
juansebastianleyva <jnleyva@gmail.com>
2022-09-24 20:07:20 -0500

    corrección problema en clase

--- src/main/java/com/usa/misiontic/masterclass3/repository/crudRepository/crudRepository/CategoryCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.usa.misiontic.masterclass3.repository.crudRepository.crudRepository;
-
-import com.usa.misiontic.masterclass3.entities.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryCrudRepository extends CrudRepository<Category,Integer> {
-}
